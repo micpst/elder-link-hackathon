6537b9c89200a2535b2e3b98c11940a16750c8cb
ParuyrGevorgyan <gevorgyan.paruyr@gmail.com>
2023-04-01 14:03:26 +0200

    algorithm (#34)

--- services/rest/rest/app/provider/views.py
+++ services/rest/rest/app/provider/views.py
@@ -4,22 +4,33 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.parsers import FileUploadParser
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.serializers import CharField, DecimalField, ListField, Serializer
+from rest_framework.status import HTTP_200_OK
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.views import APIView
 
 from rest.app.provider.models import Provider
 from rest.app.provider.serializer import ProviderSerializer
+from rest.app.provider.services import Filter, MatchingService
 
 
 class ProviderListView(ListCreateAPIView):
+    class FilterSerializer(Serializer):
+        activities = ListField(child=CharField())
+        longitude = DecimalField(max_digits=9, decimal_places=7)
+        latitude = DecimalField(max_digits=9, decimal_places=7)
+
     queryset = Provider.objects.all()
     serializer_class = ProviderSerializer
 
-    def get(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.serializer_class(queryset, many=True)
-        providers = serializer.data
-        return Response(providers)
+    def get(self, request, *args, **kwargs) -> Response:
+        filters_serializer = self.FilterSerializer(data=request.query_params)
+        filters_serializer.is_valid(raise_exception=True)
+
+        filtered_queryset = MatchingService.match(filters=Filter(**filters_serializer.validated_data))
+
+        serializer = self.serializer_class(filtered_queryset, many=True)
+        return Response(data=serializer.data, status=HTTP_200_OK)
 
 
 class ProviderDetailsView(RetrieveUpdateDestroyAPIView):
