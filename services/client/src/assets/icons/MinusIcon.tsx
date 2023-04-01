2e7e6a718c1a0b537d9e957c8535cb613752852e
Patryk Sztuczka <73931963+patryksztuczka@users.noreply.github.com>
2023-04-01 11:57:12 +0200

    feature: Added activities form (#30)

---
+++ services/client/src/assets/icons/MinusIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MinusIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+      <path
+        className="fill-green-400"
+        d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"
+      />
+    </svg>
+  );
+};
+
+export default MinusIcon;
