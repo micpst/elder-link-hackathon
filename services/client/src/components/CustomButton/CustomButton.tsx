230ffc4ba1632ed159a8a4ac3b06237b8e7b37cf
Patryk Sztuczka <73931963+patryksztuczka@users.noreply.github.com>
2023-04-01 06:05:14 +0200

    feature: change landing page and add routing (#21)

--- services/client/src/components/CustomButton/CustomButton.tsx
+++ services/client/src/components/CustomButton/CustomButton.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import { IButtonProps } from './CustomButtonTypes'
-
+import React from 'react';
+import { IButtonProps } from './CustomButtonTypes';
 
 const CustomButton = ({ text, onClick }: IButtonProps) => {
-    return (
-        <button onClick={onClick} className="bg-white uppercase text-3xl rounded-lg mx-5 py-5 mb-10 shadow-md border border-gray-300 md:h-64 md:w-96 md:text-6xl md:mb-0 md:mx-0">{text}</button>
-    )
-}
+  return (
+    <button
+      onClick={onClick}
+      className="bg-white uppercase text-3xl font-semibold hover:bg-green-400 hover:text-white rounded-lg mx-5 py-5 mb-10 shadow-md border border-gray-300 md:h-64 md:w-96 md:text-6xl md:mb-0 md:mx-0"
+    >
+      {text}
+    </button>
+  );
+};
 
-export default CustomButton
+export default CustomButton;
