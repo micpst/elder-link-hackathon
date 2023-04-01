312eb2a7d26c1b060592967695ff2d4c5dfb9785
ParuyrGevorgyan <gevorgyan.paruyr@gmail.com>
2023-04-01 03:04:53 +0200

    12 backend implement algorithm for senior provider matching (#15)
    * added provider fixtures
    
    * Initial UNFINISHED algo and get funciton for providers

--- services/rest/docker-entrypoint.sh
+++ services/rest/docker-entrypoint.sh
@@ -2,5 +2,5 @@
 
 python manage.py flush --no-input
 python manage.py migrate
-python manage.py loaddata users
+python manage.py loaddata users providers
 python manage.py runserver 0.0.0.0:8000
--- services/rest/rest/app/provider/views.py
+++ services/rest/rest/app/provider/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
 
 from rest.app.provider.models import Provider
 from rest.app.provider.serializer import ProviderSerializer
@@ -7,6 +8,11 @@ from rest.app.provider.serializer import ProviderSerializer
 class ProviderListView(ListCreateAPIView):
     queryset = Provider.objects.all()
     serializer_class = ProviderSerializer
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.serializer_class(queryset, many=True)
+        providers = serializer.data
+        return Response(providers)
 
 
 class ProviderDetailsView(RetrieveUpdateDestroyAPIView):
